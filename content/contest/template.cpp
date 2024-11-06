325f51d89a4dbcbd8cac04ffe07659ddf074590d
Hagry <mohamedelhagery@gmail.com>
2024-11-06 23:01:25 +0200

    Added basic

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,33 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define pb push_back
+#define F first
+#define S second
+#define MP make_pair
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+#define all(x) x.begin(),x.end()
+#define FAST ios::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vpi = vector <pair<int, int>>;
+using vvi = vector <vector<int>>;
+
+const int OO = 1e9 + 5;
+const int N = 2e5 + 5;
+
+void TC(){
+
+}
+
+int32_t main() {
+    FAST
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        TC();
+    }
+    return 0;
 }
