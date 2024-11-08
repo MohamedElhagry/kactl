4934870055a2f9770bc4a705cadbc93b91c6ce04
Hagry <mohamedelhagery@gmail.com>
2024-11-09 00:45:22 +0200

    Added linear algebra stuff

--- content/linear-algebra/XORBasis.cpp
+++ content/linear-algebra/XORBasis.cpp
@@ -2,22 +2,32 @@
  * Author: Gamal
  * Description: XOR Basis
  */
-const int LG = 60 + 1;
-//basis[i] contains a basis whose highest bit is i
-ll basis[LG];
-int sz;
-void insert(ll x) {
-    for (int b = LG - 1; b >= 0; --b) {
-        //dimension is 0
-        if (((1ll << b) & x) == 0)
-            continue;
-        //basis is not occupied, just put it here
-        if (basis[b] == 0) {
-            basis[b] = x;
-            ++sz;
-            return;
-        }
-        //subtract this basis from x
-        x ^= basis[b];
+struct XorBasis {
+    vector <ll> basis;
+    int LOG, sz;
+    XorBasis(int log = 61) {
+        LOG = log;
+        sz = 0;
+        basis.resize(LOG);
     }
-}
+    bool insert(ll x) {
+        for (int i = LOG - 1; i >= 0; --i) {
+            if (x >> i & 1 ^ 1)continue;
+            if (basis[i]) {
+                x ^= basis[i];
+            } else {
+                basis[i] = x;
+                sz++;
+                return true;
+            }
+        }
+        return false;
+    }
+    ll getMax(ll x = 0) {
+        ll ret = x;
+        for (int i = LOG - 1; i >= 0; --i) {
+            ret = max(ret, ret ^ basis[i]);
+        }
+        return ret;
+    }
+};
